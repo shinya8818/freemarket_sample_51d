1ea64856f29e9a8d32174a69f500ceb53595ee4c
yusukehino <abcdefghino6010@gmail.com>
2019-05-30 19:06:03 +0900

    delete-debug

--- app/assets/javascripts/items_new.js
+++ app/assets/javascripts/items_new.js
@@ -4,7 +4,6 @@ $(function(){
     //300円以上9,999,999円以下の場合のみ表示
     if(300 <= input &&  input <= 9999999){
       var handling_fee = Math.floor(input/10);
-      console.log(handling_fee);
       var profit = input - handling_fee;
       $("#handling-fee__input").text(handling_fee); 
       $("#profit__input").text(profit); 
