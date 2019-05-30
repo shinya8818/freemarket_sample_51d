a7b358e3da91e734f215b971ca1b1f42ad72952c
shinya8818 <wacker8818@outlook.com>
2019-05-30 10:19:32 +0900

    unisorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,12 @@
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 
-working_directory "#{app_path}/current"
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+working_directory app_path
+pid "#{app_path}/tmp/pids/unicorn.pid"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
@@ -23,7 +23,7 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 
   if run_once
-    run_once = false # prevent from firing again
+    run_once = false
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
