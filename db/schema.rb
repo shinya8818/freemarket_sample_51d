9aaa9059a4a500e4ea7a33dcd2edfb7ea4c968a3
shinya8818 <wacker8818@outlook.com>
2019-05-31 10:39:11 +0900

    item.column追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_105612) do
+ActiveRecord::Schema.define(version: 2019_05_31_013713) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 2019_05_30_105612) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "size"
+    t.string "shipping_fee", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
