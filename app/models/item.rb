50acd75adfcacd60ff61b4ed955eeb1c891d00f3
yusukehino <abcdefghino6010@gmail.com>
2019-06-03 12:12:45 +0900

    add-assosietion

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :categories
-  has_many :images
+  has_many :images, dependent: :destroy
   has_many :comments
   has_many :likes
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :categories
 end
