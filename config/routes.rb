acaa8277634dca41914b2117fe5ce6a083c37458
siann55 <takodo_exword22@yahoo.co.jp>
2019-06-04 14:48:55 +0900

    ユーザー登録&ログイン機能_conflict解消後に反映

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,14 @@ Rails.application.routes.draw do
     resources :categories, only: [:create]
   end
   resources :users, only: [:index, :new, :show]
+  resource :user_info_keep, to: "sessions#user_info_keep", only: :create
+  resource :phone_entrypage, to: "sessions#phone_entrypage", only: :new
+  resource :phone_info_keep, to: "sessions#phone_info_keep", only: :create
+  resource :address_entrypage, to: "sessions#address_entrypage", only: :new
+  resource :address_info_keep, to: "sessions#address_info_keep", only: :create
+  resource :credit_entrypage, to: "sessions#credit_entrypage", only: :new
+  resource :register, to: "sessions#register", only: :create
+  resource :register_done, to: "sessions#register_done", only: :new
+  resource :login, to:"users#login",only: :create
 
 end
