88b7834a9aa16d36af20073f1c3ab2f98128de68
shinya8818 <wacker8818@outlook.com>
2019-06-05 11:29:46 +0900

    migrate

--- db/migrate/20190531033850_remove_shipping_fee_from_items.rb
+++ db/migrate/20190531033850_remove_shipping_fee_from_items.rb
@@ -1,5 +1,5 @@
-# class RemoveShippingFeeFromItems < ActiveRecord::Migration[5.2]
-#   def change
-#     remove_column :items, :shipping_fee, :string
-#   end
-# end
+class RemoveShipping_FeeFromItems < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :items, :shipping_fee, :string
+  end
+end
