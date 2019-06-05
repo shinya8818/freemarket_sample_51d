da0e22ff0afe345484536bf74916a191e6274519
shinya8818 <wacker8818@outlook.com>
2019-06-05 16:48:37 +0900

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,6 @@
 class ItemsController < ApplicationController
 
   def new
-    @item = Item.new
-    @item.images.build
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      # Todo モデルに移す 画像保存処理
-      params[:item_images]['name'].each do |i|
-        @item_image =  @item.images.create!(image: i)
-      end
-      redirect_to root_path
-    else
-      render :new
-    end
   end
 
   def show
@@ -26,12 +11,6 @@ class ItemsController < ApplicationController
   def buy
     @item = Item.find(params[:id])
     @images = @item.images
-    @users = @item.user
-  end
-
-  private
-  def item_params
-    params.require(:item).permit(:name, :description, :brand, :delivery, :status, :fee, :condition, :prefecture, :days,  :user_id, :size, :shipping_fee, :category_id, images_attributes:[:image])
   end
 
 end
