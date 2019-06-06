b51d9402dec277fd7a93c723cb160c551604d006
yusukehino <abcdefghino6010@gmail.com>
2019-06-06 15:54:16 +0900

    add-select__category

--- app/assets/javascripts/item-image.js
+++ app/assets/javascripts/item-image.js
@@ -5,22 +5,22 @@ $(document).on('turbolinks:load', function() {
                </li>`
     return html;
     }
-    $('#item_images_attributes_0_image').change(function() {
-      var preview = document.querySelector('sell-upload__images-0');
-      var file = document.querySelector('input[type=file]').files[0];
-      var formData = new FormData($(this).get(0));
-      console.log($(this))
-      //ajax通信
-      $.ajax({
-        url: window.location.href,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(){
-        console.log("OK")
-      })
-  })
+    // $('#item_images_attributes_0_image').change(function() {
+    //   var preview = document.querySelector('sell-upload__images-0');
+    //   var file = document.querySelector('input[type=file]').files[0];
+    //   var formData = new FormData($(this).get(0));
+    //   console.log($(this))
+    //   //ajax通信
+    //   $.ajax({
+    //     url: window.location.href,
+    //     type: "POST",
+    //     data: formData,
+    //     dataType: 'json',
+    //     processData: false,
+    //     contentType: false
+    //   })
+    //   .done(function(){
+    //     console.log("OK")
+    //   })
+  // })
 });
