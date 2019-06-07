b68ed70758c3a6fb8d8bd20e7589c522b8aab7ed
yusukehino <abcdefghino6010@gmail.com>
2019-06-07 16:38:07 +0900

    add-function__display--many-images

--- app/assets/javascripts/item-image.js
+++ app/assets/javascripts/item-image.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function() {
   function build_image(alt, src){
-    var html = `<div>
+    var html = `<li>
                   <img alt="${alt}" src="${src}">
-               </div>`
+               </li>`
     return html;
     }
     var count = 1;
@@ -14,29 +14,31 @@ $(document).on('turbolinks:load', function() {
     // image_file.on('click', function() {
     //   image_file.children('input[name="images[name][' + image_count + ']"]')[0].click();
     // })
+    //画像の
+    var inputId = "#item_images_attributes_0_image"
     function build_file_tag(num) {
       var html = `<input name="images[name][${num}]" style="display: none;" type="file" id="image${num}">`
       return html;
     }
-
     // プレビュー
-    $('#item_images_attributes_0_image').change( $('#item_images_attributes_0_image').children('input[name="images[name][' + count + ']"]'), function (e) {
+    $(inputId).change( $('inputId').children('input[name="images[name][' + count + ']"]'), function (e) {
       // ファイルオブジェクト取得
-      var file = e.target.files[0];
-      var file_reader = new FileReader();
-      // 選択画像プレビュー操作
-      file_reader.onload = (function () {
-        return function (e) {
-          // imgタグ追加
-          image_tag = build_image(count, e.target.result);
-          $('.sell-upload__images-0').append(image_tag);
-          // ファイル数カウント
-          count += 1;
-          file_tag = build_file_tag(count);
-          $('#item_images_attributes_0_image').append(file_tag);
-        };
-      })(file);
-      file_reader.readAsDataURL(file);
+      var files = $(this).prop('files');
+      for (var i=0; i<files.length; i++) {
+        var file = e.target.files[i];
+        var file_reader = new FileReader();
+        // 選択画像プレビュー操作
+        file_reader.onload = (function () {
+          return function (e) {
+            // imgタグ追加
+            image_tag = build_image(count, e.target.result);
+            $('.sell-upload__images-0').append(image_tag);
+            // ファイル数カウント
+            count += 1;
+          };
+        })(file);
+        file_reader.readAsDataURL(file);
+      }
   });
     //ajax通信
     // $.ajax({
