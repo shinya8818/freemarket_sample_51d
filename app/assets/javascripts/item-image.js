602c972468d9c37c25d271d164cb71d8b94879a5
yusukehino <abcdefghino6010@gmail.com>
2019-06-08 18:50:09 +0900

    add-function-images__drag&drop

--- app/assets/javascripts/item-image.js
+++ app/assets/javascripts/item-image.js
@@ -7,42 +7,36 @@ $(document).on('turbolinks:load', function() {
                     <a class="item-image__btn-delete">削除</a>
                   </div>
                </li>`
-    return html;
+  return html;
+  }
+  var files_array= []
+  var count = 1;
+  var image_tag;
+  var inputId = "#item_images_attributes_0_image"
+
+  // プレビュー
+  $(inputId).change( $('inputId').children('input[name="images[name][' + count + ']"]'), function (e) {
+    // ファイルオブジェクト取得
+    var files = $(this).prop('files');
+    for (var i=0; i<files.length; i++) {
+      var file = e.target.files[i];
+      var file_reader = new FileReader();
+      //
+      files_array.push(files[i]);
+      // 選択画像プレビュー操作
+      file_reader.onload = (function () {
+        return function (e) {
+          // imgタグ追加
+          image_tag = build_image(count, e.target.result);
+          $('.sell-upload__images-0').append(image_tag);
+          // ファイル数カウント
+          count += 1;
+        };
+      })(file);
+    file_reader.readAsDataURL(file);
     }
-    var files_array= []
-    var count = 1;
-    var image_tag;
-    var inputId = "#item_images_attributes_0_image"
-    // // ファイル選択
-    // $('#item_images_attributes_0_image').on('click', function() {
-    //   $('#item_images_attributes_0_image').children('input[name="images[name][' + count + ']"]')[0].click();
-    // })
-    // image_file.on('click', function() {
-    //   image_file.children('input[name="images[name][' + image_count + ']"]')[0].click();
-    // })
-    // プレビュー
-    $(inputId).change( $('inputId').children('input[name="images[name][' + count + ']"]'), function (e) {
-      // ファイルオブジェクト取得
-      var files = $(this).prop('files');
-      for (var i=0; i<files.length; i++) {
-        var file = e.target.files[i];
-        var file_reader = new FileReader();
-        //
-        files_array.push(files[i]);
-        // 選択画像プレビュー操作
-        file_reader.onload = (function () {
-          return function (e) {
-            // imgタグ追加
-            image_tag = build_image(count, e.target.result);
-            $('.sell-upload__images-0').append(image_tag);
-            // ファイル数カウント
-            count += 1;
-          };
-        })(file);
-        file_reader.readAsDataURL(file);
-      }
   });
-  //削除機能
+//削除機能
   $(document).on('click','.item-image__btn-delete', function(){
     // index関数を利用して、クリックされたaタグが、div内で何番目のものか特定する。
     var index = $(".item-image__btn-delete").index(this);
@@ -52,6 +46,28 @@ $(document).on('turbolinks:load', function() {
     // クリックされたaタグが含まれるli要素をHTMLから削除する。
     $(this).parent().parent().remove();
   });
+  //
+  var fileArea = document.getElementById(".sell-upload__drop-box-0")
+  var inputArea = document.getElementById("#item_images_attributes_0_image")
+  $(document).on('drop', fileArea, function(e){
+    fileArea.addEventListener('drop', (e) => {
+      e.preventDefault();
+      body.classList.remove('dragging');
+      inputArea.files = e.dataTransfer.files;
+    });
+  })
+
+  $(".sell-upload__drop-box-0").change(function(e){
+    e.preventDefault
+    dropp
+  })
+  function dropp(){
+    const droparea = document.getElementById('.sell-upload__drop-box-0');
+    droparea.addEventListener('drop', function(evt) {
+      console.log('dropしたよ');
+      /** 以下、drop後の処理 */
+    }, true);
+  }
     //ajax通信
     // $.ajax({
     //   url: url,
