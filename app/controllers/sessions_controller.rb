709384873f42333511ed50e46af270d30c575a90
siann55 <takodo_exword22@yahoo.co.jp>
2019-06-10 12:01:51 +0900

    コンフリクト修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,6 @@ class SessionsController < Devise::SessionsController
     session[:birth_year] = params[:birth_year]
     session[:birth_month] = params[:birth_month]
     session[:birth_day] = params[:birth_day]
-  test = User.new(nickname: session[:nickname])
 
     if session[:nickname].blank? || 
        session[:email].blank? || 
@@ -48,6 +47,7 @@ class SessionsController < Devise::SessionsController
   end
 
   def address_info_keep
+    
     session[:addr_last_name] = params[:addr_last_name]
     session[:addr_first_name] = params[:addr_first_name]
     session[:addr_last_name_kana] = params[:addr_last_name_kana]
@@ -59,38 +59,49 @@ class SessionsController < Devise::SessionsController
     session[:building] = params[:building]
     session[:addr_cell_phone] = params[:addr_cell_phone]
 
-    User.create!(
-      nickname: session[:nickname],
-      email: session[:email],
-      password: session[:password],
-      password_confirmation: session[:password_confirmation],
-      last_name: session[:last_name],
-      first_name: session[:first_name],
-      last_name_kana: session[:last_name_kana],
-      first_name_kana: session[:first_name_kana],
-      birth_year: session[:birth_year],
-      birth_month: session[:birth_month],
-      birth_day: session[:birth_day],
-      cell_phone: session[:cell_phone]
-    )
-
-    user = User.find_by(email: session[:email])
-    Address.create(
-      last_name: session[:addr_last_name],
-      first_name: session[:addr_first_name],
-      last_name_kana: session[:addr_last_name_kana],
-      first_name_kana: session[:addr_first_name_kana],
-      postal_code: session[:postal_code],
-      prefecture: session[:prefecture],
-      municipalities: session[:municipalities],
-      address_number: session[:address_number],
-      building: session[:building],
-      cell_phone: session[:addr_cell_phone],
-      user_id: user.id
-    )
-    sign_in user
-
-    redirect_to new_card_path
+    if session[:addr_last_name].blank? || 
+      session[:addr_first_name].blank? || 
+      session[:addr_last_name_kana].blank? ||
+      session[:addr_first_name_kana].blank? ||
+      session[:postal_code].blank? ||
+      session[:prefecture].blank? ||
+      session[:municipalities].blank? ||
+      session[:address_number].blank?
+      # JSで入力エラー表示
+    else
+      User.create!(
+        nickname: session[:nickname],
+        email: session[:email],
+        password: session[:password],
+        password_confirmation: session[:password_confirmation],
+        last_name: session[:last_name],
+        first_name: session[:first_name],
+        last_name_kana: session[:last_name_kana],
+        first_name_kana: session[:first_name_kana],
+        birth_year: session[:birth_year],
+        birth_month: session[:birth_month],
+        birth_day: session[:birth_day],
+        cell_phone: session[:cell_phone]
+      )
+  
+      user = User.find_by(email: session[:email])
+      Address.create(
+        last_name: session[:addr_last_name],
+        first_name: session[:addr_first_name],
+        last_name_kana: session[:addr_last_name_kana],
+        first_name_kana: session[:addr_first_name_kana],
+        postal_code: session[:postal_code],
+        prefecture: session[:prefecture],
+        municipalities: session[:municipalities],
+        address_number: session[:address_number],
+        building: session[:building],
+        cell_phone: session[:addr_cell_phone],
+        user_id: user.id
+      )
+      sign_in user
+  
+      redirect_to new_card_path
+    end
   end
 
   def credit_entrypage
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
+  devise_for :users, controllers: { sessions: "sessions", registrations: "registrations" }
   get 'purchase/index'
   get 'purchase/done'
   get 'card/new'
   get 'card/show'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   # get 'buy/items/:id' => 'items#buy'
