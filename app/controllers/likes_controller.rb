97797a3fa54847a71d9fbe3c5046b38a29451b4b
shinya8818 <wacker8818@outlook.com>
2019-06-12 18:59:40 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,20 +1,16 @@
 class LikesController < ApplicationController
 
-  before_action :get_item
-
   def create
-    @like = Like.new(user_id: current_user.id, item_id: params[:item_id])
-    if @like.save
-      @likes = Like.where(item_id: params[:item_id])
-    else
-      redirect_to item_path
-    end
+    @like = Like.create(user_id: current_user.id, item_id: params[:item_id])
+    @likes = Like.where(item_id: params[:item_id])
+    get_item
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, item_id: params[:item_id])
     @like.destroy
     @likes = Like.where(item_id: params[:item_id])
+    get_item
   end
 
   def get_item
