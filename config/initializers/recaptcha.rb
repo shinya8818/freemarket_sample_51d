1021dc48d29a65ce736d48155adbaf8ffd925bf7
shinya8818 <wacker8818@outlook.com>
2019-06-18 18:40:22 +0900

    commit

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  # config.site_key = Rails.application.credentials.recaptcha[:access_key_id]
-  # config.secret_key = Rails.application.credentials.recaptcha[:secret_access_key]
+  config.site_key = ENV [:access_key_id]
+  config.secret_key = ENV [:secret_access_key]
 end
