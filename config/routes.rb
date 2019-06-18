c11330fe09433ce9b99dd9696cfeaab48fe6f465
matsushitaseiya <ankle.break.ayies@gmail.com>
2019-06-18 14:55:04 +0900

    sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,8 @@ Rails.application.routes.draw do
   resource :login, to:"users#login",only: :create
   resource :logout, to:"users#logout"
   resource :exhibition, to:"users#exhibition"
+  resource :purchaselist, to:"users#purchaselist"
+  resource :purchasedlist, to:"users#purchased"
   resource :identity, to:"users#identity"
   resource :profile, to:"users#profile"
   resource :complete, to:"users#complete"
