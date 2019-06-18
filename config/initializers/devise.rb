be47507e83ef68d30cfa562ca8feed80a6b1fd06
shinya8818 <wacker8818@outlook.com>
2019-06-18 16:41:54 +0900

    grm

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,8 +259,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, Rails.application.credentials.facebook[:access_key_id],Rails.application.credentials.facebook[:secret_access_key]
-  config.omniauth :google_oauth2, Rails.application.credentials.google[:access_key_id],Rails.application.credentials.google[:secret_access_key]
+  # config.omniauth :facebook, Rails.application.credentials.facebook[:access_key_id],Rails.application.credentials.facebook[:secret_access_key]
+  # config.omniauth :google_oauth2, Rails.application.credentials.google[:access_key_id],Rails.application.credentials.google[:secret_access_key]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = Rails.application.credentials.recaptcha[:access_key_id]
-  config.secret_key = Rails.application.credentials.recaptcha[:secret_access_key]
+  # config.site_key = Rails.application.credentials.recaptcha[:access_key_id]
+  # config.secret_key = Rails.application.credentials.recaptcha[:secret_access_key]
 end
-
