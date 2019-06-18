a91a40ec5d4a574120442f9fd760205485082649
shinya8818 <wacker8818@outlook.com>
2019-06-18 18:55:04 +0900

    Update recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = "6LdyYKgUAAAAAPc1eKfq7Uv4qabOqWNk4pF99L48"
-  config.secret_key = "6LdyYKgUAAAAAMExnrdQfe5H2Kr-aIfZyA5Tylge"
+  config.site_key = "6LdIbakUAAAAAE5GqHj8Gm1HUGuJDoj4F9Jmkix7"
+  config.secret_key = "6LdIbakUAAAAAL7_US8BMcbL21k-2SuEh4xSKMa6"
 end
