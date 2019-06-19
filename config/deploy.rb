22c45e46b852d94eda5d67b9da2e28586b5e0474
shinya8818 <wacker8818@outlook.com>
2019-06-19 17:35:38 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,9 @@ set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
+  aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+  aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
 }
 
 set :application, 'freemarket_sample_51d'
